362715f34d6ea68b7fb95e27b7188bcb5ecb112c
bethanyr <bethany.rentz@hotmail.com>
2015-04-01 17:31:28 -0700

    copy of files from carnesmedia

---
+++ Rakefile
@@ -0,0 +1,2 @@
+require "bundler/gem_tasks"
+require './lib/resque/plugins/heroku/tasks'
---
+++ lib/resque/plugins/heroku/version.rb
@@ -0,0 +1,7 @@
+module Resque
+  module Plugins
+    module Heroku
+      VERSION = "0.1.0"
+    end
+  end
+end
